517222bedd938bbcdf62af4358b2dc77c9c2e6e6
pranita.panigrahi <pranita_panigrahi@epam.com>
2024-06-14 06:57:22 +0530

    removed ununsed files

--- tiny-apps/learn-js/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Display } from './pages/Display';
-
-export const App = () => {
-  return <div><Display/></div>;
-};
--- tiny-apps/learn-js/src/pages/Display.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DisplayList from '../components/DisplayList';
-
-export const Display = () => {
-  return <DisplayList />;
-};
